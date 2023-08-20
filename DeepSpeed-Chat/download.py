c8d8067c0cc106b13b8928f6374df2d596281715
LuYF-Lemon-love <3555028709@qq.com>
2023-08-20 23:12:04 +0800

    修改代码

---
+++ DeepSpeed-Chat/download.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+#
+# DeepSpeed-Chat/download.py
+#
+# created by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on August 19, 2023
+#
+# 下载模型和数据.
+
+# from huggingface_hub import snapshot_download
+
+# snapshot_download(repo_id="facebook/opt-1.3b", local_dir="opt-1.3b")
+
+# snapshot_download(repo_id="Dahoas/rm-static", repo_type="dataset", local_dir="rm-static")
+
+# python model_download.py --repo_id facebook/opt-1.3b --mirror
+# python model_download.py --repo_id Dahoas/rm-static --mirror --repo_type dataset
