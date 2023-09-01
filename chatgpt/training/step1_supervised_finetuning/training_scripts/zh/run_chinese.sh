cd447e3f5f0b2a2ec1fb7e5475b7676953e69544
LuYF-Lemon-love <3555028709@qq.com>
2023-09-01 13:16:52 +0800

    汉化修改

--- chatgpt/training/step1_supervised_finetuning/training_scripts/zh/run_chinese.sh
+++ chatgpt/training/step1_supervised_finetuning/training_scripts/zh/run_chinese.sh
@@ -7,49 +7,26 @@
 #
 # 训练脚本.
 
-# OUTPUT=$1
-# ZERO_STAGE=$2
-# if [ "$OUTPUT" == "" ]; then
-#     OUTPUT=./output
-# fi
-# if [ "$ZERO_STAGE" == "" ]; then
-#     ZERO_STAGE=0
-# fi
-# mkdir -p $OUTPUT
-
-# deepspeed --num_gpus 2 main.py --model_name_or_path ../../bloom-1b1 \
-#    --data_path ../../rm-static-zh \
-#    --per_device_train_batch_size 4 \
-#    --per_device_eval_batch_size 4 \
-#    --gradient_accumulation_steps 8 --lora_dim 128 --zero_stage $ZERO_STAGE \
-#    --enable_tensorboard \
-#    --tensorboard_path $OUTPUT \
-#    --deepspeed --output_dir $OUTPUT &> $OUTPUT/training.log
-
 OUTPUT=$1
 ZERO_STAGE=$2
 if [ "$OUTPUT" == "" ]; then
     OUTPUT=./output
 fi
 if [ "$ZERO_STAGE" == "" ]; then
-    ZERO_STAGE=2
+    ZERO_STAGE=3
 fi
 mkdir -p $OUTPUT
 
-# --data_path ../../Zhihu-KOL ../../HC3-Chinese \
-deepspeed --num_gpus 2 main.py \
+deepspeed --num_gpus 1 main.py \
    --model_name_or_path ../../bloom-1b1 \
    --data_path ../../rm-static-zh \
    --per_device_train_batch_size 4 \
    --per_device_eval_batch_size 4 \
-   --learning_rate 9.65e-6 \
-   --weight_decay 0. \
-   --num_train_epochs 16 \
-   --gradient_accumulation_steps 1 \
-   --lr_scheduler_type cosine \
-   --num_warmup_steps 0 \
-   --seed 1234 \
+   --gradient_accumulation_steps 8 \
+   --num_train_epochs 1 \
    --zero_stage $ZERO_STAGE \
+   --enable_tensorboard \
+   --tensorboard_path $OUTPUT \
    --deepspeed \
    --output_dir $OUTPUT \
    &> $OUTPUT/training.log
